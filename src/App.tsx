8c4202cdb2ece881f3c5233fb4742489d2a52661
Oleh Buhaichuk <oleh.buhaichuk@gmail.com>
2023-09-26 19:54:33 +0300

    add redux to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,8 @@
 import './App.css';
-import { Child, ChildFC } from './components/Child';
-import EventComponent from './events/EventComponent';
-import GuestList from './state/GuestList';
-import UserSearch from './state/UserSearch';
-
 function App() {
-  const handleClick = () => {
-    console.log('Click');
-  };
-
   return (
     <>
       <h2>Hi there</h2>
-      <Child textColor={'blue'} onClick={handleClick}></Child>
-      <ChildFC textColor={'red'} onClick={handleClick} />
-      <GuestList />
-      <UserSearch />
-
-      <EventComponent />
     </>
   );
 }
